bd12a2aae58192f5ff29bfc37cf326bb35be4be9
David Endersby <daendersby@gmail.com>
2017-08-20 13:56:28 +0200

    Ok So I did a few things, mainly to make the animations a little nicer - The main thing I wanted to do was to make animations work even when the mouse was outside of the block.   - Removed the hide class (from the css and the svg markup)   - Display none now applied directly to bite   - So turnbrown and turngreen now go to brown/green and back in one animation (technically its 2 iterations running back to back in an alternate direction). You could've also edited the animation block to have 3 states - Original State = 0%, other state= 50% and Original State = 100% but this is more re-usible   - Removed all but one instances of $(document).ready(). You only need this once.   - Swapped mouseleave event listener for animationEnd   - Put some selectors into variables   - Added specific selectors on cards as identifiers (like lateNightSnack)
    Note: Consider using camelCase in js and dash-case for css

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,37 +1,43 @@
 /*CV CLICK INTERACTION*/
+// So you only need to call document.ready once.
 $(document).ready(function () {
-	
+
 	var details = $(this).closest('.details');
-	
+
 	$(".cventry").on("click", function() {
 		$(this).nextAll(".details").slideToggle();
 	});
 
-});
+	/*SVG ANIMATIONS LANDINGPAGE*/
 
-/*SVG ANIMATIONS LANDINGPAGE*/
+	/*SUN*/
 
-/*SUN*/
-$(document).ready(function () {
+	// Storing elements in variables makes the js faster as the js doesn't have to go n find the element in the DOM every time it needs to modify it. The general rule is if you use the element more than once, it should be a variable.
+	var cloud = $(".cloud");
+	var drop = $(".drop");
 
-	$(".brightblue").on("mouseenter", function() {	
-		$(".cloud").removeClass("hide");
-		$(".drop").removeClass("hide");
-		$(".cloud").addClass("active");
-		$(".drop").addClass("rain");
+	$(".amssunmachine").on("mouseenter", function() {
+		// If the cloud element isn't active, then we add the can animate it
+		if (!cloud.hasClass("active")) {
+			cloud.addClass("active");
+			drop.addClass("active");
+		}
 	});
 
-	$(".brightblue").on("mouseleave", function() {
-		$(".cloud").addClass("hide");
-		$(".drop").addClass("hide");
-	});
+	// We listen to the animationEnd event which is fired when the animation finishes. This means the animation will keep running even when the mouse leaves the box. This means theres no jerk. Woop!
+	cloud.bind("webkitAnimationEnd mozAnimationEnd animationEnd", function(){
+		// If the cloud element is animating, is it active and we can remove the classes
+		if (cloud.hasClass("active")) {
+			cloud.removeClass("active");
+			drop.removeClass("active");
+		}
+	})
 
-});
+	/*BURGER*/
+	// Normally we bundle all vars at the top but i'll leave this one ere for readability
+	var lateNightSnack = $(".latenightsnack");
 
-/*BURGER*/
-$(document).ready(function () {
-
-	$(".darkblue").on("mouseenter", function() {
+	lateNightSnack.on("mouseenter", function() {
 		$(".topgroup").addClass("active");
 		$(".patty").addClass("active");
 		$(".saladgroup").addClass("active");
@@ -39,27 +45,23 @@ $(document).ready(function () {
 		$(".bite").addClass("active");
 	});
 
-	$(".darkblue").on("mouseleave", function() {
+	lateNightSnack.on("webkitAnimationEnd mozAnimationEnd animationEnd", function() {
 		$(".topgroup").removeClass("active");
 		$(".patty").removeClass("active");
 		$(".saladgroup").removeClass("active");
 		$(".bottombun").removeClass("active");
 		$(".bite").removeClass("active");
-		$(".bite").removeClass("hide");
 	});
 
-});
+	/*AVOCADO*/
 
-/*AVOCADO*/
-$(document).ready(function () {
-
-	$(".brightgreen").on("mouseenter", function() {		
+	$(".brightgreen").on("mouseenter", function() {
 		$(".avocado").addClass("rotate");
 		$(".green").addClass("turnbrown");
 		$(".brown").addClass("turngreen");
 	});
 
-	$(".brightgreen").on("mouseleave", function() {
+	$(".brightgreen").on("webkitAnimationEnd mozAnimationEnd animationEnd", function() {
 		$(".avocado").removeClass("rotate");
 		$(".green").removeClass("turnbrown");
 		$(".brown").removeClass("turngreen");
